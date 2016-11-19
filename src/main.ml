90003f4ad2a27cfc596f4d24cbdc8f14fb5657ae
Mindy Preston <mindy.preston@docker.com>
2016-11-19 16:02:17 -0600

    take a file argument

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,7 @@
 open OpamParserTypes
+open Cmdliner
 
-let find_mirage contents =
+let find_mirage file =
   let any ~str = function
   | String (_, name) when name = str -> true
   | Option (_, String (_pos, name), _ops) when name = str -> true
@@ -11,8 +12,22 @@ let find_mirage contents =
   | Variable (_, "depends", List (_, deps)) -> List.exists (any ~str:"mirage-types") deps || List.exists (any ~str:"mirage-types-lwt") deps
   | Variable _ -> false
   in
-  List.exists relevant contents
+  let e = List.exists relevant @@ (OpamParser.file file).file_contents in
+  e
+
+let file =
+  let doc = "The OPAM file to check for Mirage dependencies." in
+  Arg.(value & pos 0 string "opam" & info [] ~doc)
+
+let info =
+  let doc = "check a file for MirageOS API dependencies" in
+  let man = [ `S "BUGS"; `P "Please report bugs at https://github.com/yomimono/upperbound-constrainer/issues"; ] in
+  Term.info "upperbound-constrainer" ~version:"0.0.1" ~doc ~man
 
 let () =
   (* opamfile is a record, containing file_contents (which we're interested in) and file_name (which we're not) *)
-  Format.printf "%s" @@ string_of_bool @@ find_mirage @@ (OpamParser.file "opam").file_contents
+  let find_t = Term.(const find_mirage $ file) in
+  match Term.eval (find_t, info) with
+  | `Ok true -> exit 0
+  | `Ok false -> exit 1
+  | _ -> exit 127
