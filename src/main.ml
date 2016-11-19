50440ba290908377ebb861d2d5e878ff6a525a11
Mindy Preston <mindy.preston@docker.com>
2016-11-19 17:21:44 -0600

    should ignore eqs too, exit code 0 & output only when we changed something

--- src/main.ml
+++ src/main.ml
@@ -3,30 +3,29 @@ open Cmdliner
 
 let upperbound = "3.0.0" (* the whole point of this exercise *)
 
+let acted = ref false
+
 let find_mirage file =
   let has_upper_bound constraints =
     (* TODO: the actual rules of these constraints are potentially complex;
      * is there a way to evaluate them rather than checking them in text? *)
-    (* things which express an upper bound constraint: LT and LEQ which are not negated,
-     * GT or GEQ which are negated (but not sure if this is valid in opam) *)
     let rec is_upper_bound = function
     | Logop (_, `And, e, r) -> is_upper_bound e || is_upper_bound r
-    | Prefix_relop (_, `Lt, _) | Prefix_relop (_, `Leq, _) -> true
+    | Prefix_relop (_, `Lt, _) | Prefix_relop (_, `Leq, _) | Prefix_relop (_, `Eq, _) -> true
     | _ -> false
     in
-    (* this isn't right -- we need to descend into positive nodes *)
     List.exists is_upper_bound constraints
   in
   let add_upper_bound = function
   (* no constraints yet; we're the first one. *)
-  | String (pos, name) -> Option (pos, (String (pos, name)), (* quite a bit more structure! *)
+  | String (pos, name) -> acted := true; Option (pos, (String (pos, name)), (* quite a bit more structure! *)
     [
       Prefix_relop (pos, `Lt,
         (String (pos, upperbound))
       )
     ]
   )
-  | Option (pos, (String (p, name)), (Prefix_relop (a, b, c))::_) ->
+  | Option (pos, (String (p, name)), (Prefix_relop (a, b, c))::_) -> acted := true;
     Option (pos, (String (p, name)), [ Logop (p, `And, Prefix_relop (a, b, c),
       Prefix_relop (pos, `Lt,
         (String (pos, upperbound))
@@ -47,21 +46,23 @@ let find_mirage file =
   | other -> other
   in
   let l = (OpamParser.file file).file_contents in
-  let new_contents = List.map transform_dependencies l in
-  Some { file_name = file; file_contents = new_contents}
+  { file_name = file; file_contents = (List.map transform_dependencies l)}
 
 let file =
-  let doc = "The OPAM file to check for Mirage dependencies." in
+  let doc = "The OPAM file to add upper bounds to." in
   Arg.(value & pos 0 string "opam" & info [] ~doc)
 
 let info =
-  let doc = "check a file for MirageOS API dependencies" in
+  let doc = "add constraints to mirage-types and mirage-types-lwt dependencies in opam files." in
   let man = [ `S "BUGS"; `P "Please report bugs at https://github.com/yomimono/upperbound-constrainer/issues"; ] in
   Term.info "upperbound-constrainer" ~version:"0.0.1" ~doc ~man
 
 let () =
-  (* opamfile is a record, containing file_contents (which we're interested in) and file_name (which we're not) *)
   let find_t = Term.(const find_mirage $ file) in
   match Term.eval (find_t, info) with
-  | `Ok (Some corrected) -> Format.printf "%s" @@ OpamPrinter.opamfile corrected
+  | `Ok file -> begin
+    match !acted with
+    | true -> Format.printf "%s\n" @@ OpamPrinter.opamfile file
+    | false -> exit 1
+  end
   | _ -> exit 1
